e507b398151e352e30b9e5958602dd765fc7293d
Vo Tran Phi <songokupkj@gmail.com>
2024-04-14 22:49:56 +0700

    feat: add sslstream and sending emoji, image to chat app

---
+++ Client/ClientForm.cs
@@ -0,0 +1,357 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class ClientForm : Form
+    {
+        private TcpClient tcpClient;
+        private Thread clientThread;
+        private bool isClientRunning = false;
+        private SslStream sslStream;
+        // maximum buffer size can read to
+        private static int buff_size = 2048;
+        private byte[] buffer = new byte[buff_size];
+        private delegate void SafeCallDelegate(string text);
+        private delegate void SafeCallDelegateImage(Bitmap bmp);
+
+        public ClientForm()
+        {
+            InitializeComponent();
+        }
+
+        // The following method is invoked by the RemoteCertificateValidationDelegate.
+        public static bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            // MessageBox.Show($"Certificate error: {sslPolicyErrors}");
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return true;
+        }
+
+        private void receiveFromServer()
+        {
+            try
+            {
+                while (isClientRunning)
+                {
+                    int readBytes = sslStream.Read(buffer);
+                    string msg = Encoding.UTF8.GetString(buffer, 0, readBytes);
+
+                    if (msg == "<Invalid_Username_Exists>")
+                    {
+                        UpdateChatHistoryThreadSafe($"Username already exists, please pick another one!\n");
+                        isClientRunning = false;
+                        UpdateConnectButtonTextThreadSafe("Connect");
+                        break;
+                    }
+                    if (msg == "<Accepted>")
+                    {
+                        UpdateChatHistoryThreadSafe($"[{DateTime.Now}] Connected to the server with ip address {ipInput.Text} on port {portInput.Text}\n");
+                        continue;
+                    }
+
+                    if (msg == "<Image>")
+                    {
+                        // maximum size of image is 524288 bytes
+                        byte[] bytes = new byte[524288];
+                        // wait for server side to complete writing data
+                        Thread.Sleep(1000);
+                        sslStream.Read(bytes);
+
+                        Bitmap bmp;
+                        using (var ms = new MemoryStream(bytes))
+                        {
+                            bmp = new Bitmap(ms);
+                        }
+
+
+                        UpdateImageThreadSafe(bmp);
+
+                        continue;
+                    }
+
+                    // the if... below resolves the Problem 1 in ServerForm.cs
+                    if (msg != null && msg != "")
+                    {
+                        UpdateChatHistoryThreadSafe($"{msg}\n");
+                    }
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                tcpClient.Close();
+                sslStream.Close();
+            }
+        }
+
+        private void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (isClientRunning)
+                {
+                    isClientRunning = false;
+                    // streamWriter.WriteLine("<Disconnect>");
+                    byte[] bufferWr = Encoding.UTF8.GetBytes("<Disconnect>");
+                    sslStream.Write(bufferWr);
+                    sslStream.Flush();
+                    tcpClient = null;
+                    statusAndMsg.Text += $"[{DateTime.Now}] Disconnected from the server with ip address {ipInput.Text} on port {portInput.Text}\n";
+                    connectBtn.Text = "Connect";
+                }
+                else
+                {
+                    isClientRunning = true;
+                    tcpClient = new TcpClient();
+                    tcpClient.Connect(new IPEndPoint(IPAddress.Parse(ipInput.Text), int.Parse(portInput.Text)));
+
+                    // Create an SSL stream that will close the client's stream.
+                    sslStream = new SslStream(
+                        tcpClient.GetStream(),
+                        false,
+                        new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                        null
+                        );
+                    // The server name must match the name on the server certificate.
+                    try
+                    {
+                        sslStream.AuthenticateAsClient("MySslSocketCertificate");
+                    }
+                    catch (AuthenticationException ex)
+                    {
+                        MessageBox.Show($"Exception: {ex.Message}");
+                        if (ex.InnerException != null)
+                        {
+                            MessageBox.Show($"Inner exception: {ex.InnerException.Message}");
+                        }
+                        MessageBox.Show("Authentication failed - closing the connection.");
+                        tcpClient.Close();
+                        return;
+                    }
+
+                    // send the username to the server first
+                    byte[] bufferWr = Encoding.UTF8.GetBytes(usernameInput.Text);
+                    sslStream.Write(bufferWr);
+                    sslStream.Flush();
+
+                    clientThread = new Thread(this.receiveFromServer);
+                    clientThread.Start();
+                    connectBtn.Text = "Disconnect";
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                MessageBox.Show(sockEx.Message, "Network error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                isClientRunning = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tcpClient != null && tcpClient.Connected)
+                {
+                    byte[] bufferWr = Encoding.UTF8.GetBytes(msgToSend.Text);
+                    sslStream.Write(bufferWr);
+                    sslStream.Flush();
+                    statusAndMsg.Text += $"[{DateTime.Now}] {usernameInput.Text}: {msgToSend.Text}\n";
+                    msgToSend.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void msgToSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (sendBtn.Enabled)
+                {
+                    sendBtn_Click(sender, e);
+                }
+            }
+        }
+
+        private void emoBtn_Click(object sender, EventArgs e)
+        {
+            msgToSend.Focus();
+        }
+
+        private void imageBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image file|*.jpg";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                // convert a image into byte array
+                Image image = Image.FromFile(ofd.FileName);
+                var ms = new MemoryStream();
+                image.Save(ms, image.RawFormat);
+                byte[] bytes = ms.ToArray();
+
+                // send the message and byte array
+                byte[] bufferWr = Encoding.UTF8.GetBytes("<Image>");
+                sslStream.Write(bufferWr);
+                sslStream.Flush();
+                sslStream.Write(bytes);
+                sslStream.Flush();
+            }
+        }
+
+        private void emojiCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            msgToSend.Text += emojiCB.Text;
+            emojiCB.SelectedIndex = -1;
+        }
+
+        #region UpdateThreadSafe
+
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (statusAndMsg.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                statusAndMsg.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                statusAndMsg.Text += text;
+            }
+        }
+
+        private void UpdateConnectButtonTextThreadSafe(string text)
+        {
+            if (connectBtn.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateConnectButtonTextThreadSafe);
+                connectBtn.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                connectBtn.Text = text;
+            }
+        }
+
+        private void UpdateImageThreadSafe(Bitmap bmp)
+        {
+            if (statusAndMsg.InvokeRequired)
+            {
+                var d = new SafeCallDelegateImage(UpdateImageThreadSafe);
+                statusAndMsg.Invoke(d, new object[] { bmp });
+            }
+            else
+            {
+                statusAndMsg.Text += "\n";
+                // move cursor to the end of the RTB
+                statusAndMsg.Select(statusAndMsg.Text.Length - 1, 0);
+                // scroll to cursor the RTB
+                statusAndMsg.ScrollToCaret();
+
+                Clipboard.SetDataObject(bmp);
+                statusAndMsg.Paste();
+            }
+        }
+
+        #endregion
+
+        #region Responsive
+
+        private void ipInput_TextChanged(object sender, EventArgs e)
+        {
+            if (ipInput.Text != "" && portInput.Text != "" && usernameInput.Text != "")
+            {
+                connectBtn.Enabled = true;
+            }
+            else
+            {
+                connectBtn.Enabled = false;
+            }
+        }
+
+        private void portInput_TextChanged(object sender, EventArgs e)
+        {
+            if (ipInput.Text != "" && portInput.Text != "" && usernameInput.Text != "")
+            {
+                connectBtn.Enabled = true;
+            }
+            else
+            {
+                connectBtn.Enabled = false;
+            }
+        }
+
+        private void usernameInput_TextChanged(object sender, EventArgs e)
+        {
+            if (ipInput.Text != "" && portInput.Text != "" && usernameInput.Text != "")
+            {
+                connectBtn.Enabled = true;
+            }
+            else
+            {
+                connectBtn.Enabled = false;
+            }
+        }
+
+        private void connectBtn_TextChanged(object sender, EventArgs e)
+        {
+            if (connectBtn.Text == "Connect")
+            {
+                ipInput.Enabled = true;
+                portInput.Enabled = true;
+                usernameInput.Enabled = true;
+                imageBtn.Enabled = false;
+            }
+            else
+            {
+                ipInput.Enabled = false;
+                portInput.Enabled = false;
+                usernameInput.Enabled = false;
+                imageBtn.Enabled = true;
+            }
+        }
+
+        private void msgToSend_TextChanged(object sender, EventArgs e)
+        {
+            if (msgToSend.Text == "")
+            {
+                sendBtn.Enabled = false;
+            }
+            else
+            {
+                if (connectBtn.Text == "Connect")
+                {
+                    sendBtn.Enabled = false;
+                }
+                else
+                {
+                    sendBtn.Enabled = true;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Server/ServerForm.cs
@@ -0,0 +1,208 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Text;
+
+namespace Server
+{
+    public partial class ServerForm : Form
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+        private bool isServerRunning = false;
+        private Dictionary<string, TcpClient> listOfClients = new Dictionary<string, TcpClient>();
+        private delegate void SafeCallDelegate(string text);
+        private delegate void SafeCallDelegateImage(Bitmap bmp);
+
+        private X509Certificate serverCertificate = null;
+        // maximum buffer size can read to
+        private static int buff_size = 2048;
+        private byte[] buffer = new byte[buff_size];
+
+        public ServerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void listen()
+        {
+            try
+            {
+                serverCertificate = new X509Certificate("Socket.pfx", "136900");
+                tcpListener = new TcpListener(new IPEndPoint(IPAddress.Parse(ipInput.Text), int.Parse(portInput.Text)));
+                tcpListener.Start();
+
+                while (isServerRunning)
+                {
+                    // accepts if there is a client
+                    TcpClient _client = tcpListener.AcceptTcpClient();
+
+                    SslStream sslStream = new SslStream(_client.GetStream(), false);
+                    sslStream.AuthenticateAsServer(serverCertificate, false, true);
+
+                    // receive the username from connected client
+                    int readBytes = sslStream.Read(buffer);
+                    string username = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                    if (listOfClients.ContainsKey(username))
+                    {
+                        byte[] bufferWr = Encoding.UTF8.GetBytes("<Invalid_Username_Exists>");
+                        sslStream.Write(bufferWr);
+                        // _client.Close();
+                    }
+                    else
+                    {
+                        byte[] bufferWr = Encoding.UTF8.GetBytes("<Accepted>");
+                        sslStream.Write(bufferWr);
+                        sslStream.Flush();
+                        Thread _clientThread = new Thread(() => this.receiveFromClient(username, _client, sslStream));
+                        listOfClients.Add(username, _client);
+                        _clientThread.Start();
+
+                        string clientIPAddress = ((IPEndPoint)_client.Client.RemoteEndPoint).Address.ToString();
+                        string clientPort = ((IPEndPoint)_client.Client.RemoteEndPoint).Port.ToString();
+                        UpdateChatHistoryThreadSafe($"[{DateTime.Now}] Accept connection from {username} ({clientIPAddress}:{clientPort})\n");
+                    }
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void receiveFromClient(string username, TcpClient _client, SslStream sslStream)
+        {
+            try
+            {
+                while (isServerRunning)
+                {
+                    int readBytes = sslStream.Read(buffer);
+                    string msgFromClient = Encoding.UTF8.GetString(buffer, 0, readBytes);
+                    if (msgFromClient == "<Disconnect>")
+                    {
+                        listOfClients.Remove(username);
+                        // Problem 1: two lines below will send the empty string to the client after close
+                        _client.Close();
+                        sslStream.Close();
+                        UpdateChatHistoryThreadSafe($"[{DateTime.Now}] {username} disconnected from server!\n");
+                        break;
+                    }
+
+                    if (msgFromClient == "<Image>")
+                    {
+                        // maximum size of image is 524288 bytes
+                        byte[] bytes = new byte[524288];
+                        // wait for client side to complete writing data
+                        Thread.Sleep(1000);
+                        sslStream.Read(bytes);
+
+                        foreach (TcpClient i in listOfClients.Values)
+                        {
+                            if (i != _client)
+                            {
+                                SslStream _sslStream = new SslStream(i.GetStream());
+                                byte[] bufferWr = Encoding.UTF8.GetBytes("<Image>");
+                                _sslStream.Write(bufferWr);
+                                _sslStream.Flush();
+                                _sslStream.Write(bytes);
+                                _sslStream.Flush();
+                            }
+                        }
+
+                        // image view
+                        Bitmap bmp;
+                        using (var ms = new MemoryStream(bytes))
+                        {
+                            bmp = new Bitmap(ms);
+                        }
+                        UpdateImageThreadSafe(bmp);
+
+                        continue;
+                    }
+
+                    string formattedMsg = $"[{DateTime.Now}] {username}: {msgFromClient}";
+
+                    // send the received message to all the clients except the incoming one
+                    foreach (TcpClient i in listOfClients.Values)
+                    {
+                        if (i != _client)
+                        {
+                            SslStream _sslStream = new SslStream(i.GetStream());
+                            byte[] bufferWr = Encoding.UTF8.GetBytes(formattedMsg);
+                            _sslStream.Write(bufferWr);
+                            _sslStream.Flush();
+                        }
+                    }
+
+                    // message view
+                    UpdateChatHistoryThreadSafe(formattedMsg + "\n");
+                }
+            }
+            catch (SocketException sockEx)
+            {
+                _client.Close();
+                sslStream.Close();
+            }
+        }
+
+        private void listenBtn_Click(object sender, EventArgs e)
+        {
+            if (isServerRunning)
+            {
+                isServerRunning = false;
+                tcpListener.Stop();
+                listenThread = null;
+                statusAndMsg.Text += $"[{DateTime.Now.ToString()}] Stop listening with ip address {ipInput.Text} on port {portInput.Text}\n";
+                listenBtn.Text = "Listen";
+            }
+            else
+            {
+                isServerRunning = true;
+                listenThread = new Thread(this.listen);
+                listenThread.Start();
+                statusAndMsg.Text += $"[{DateTime.Now.ToString()}] Start listening with ip address {ipInput.Text} on port {portInput.Text}\n";
+                listenBtn.Text = "Stop";
+            }
+        }
+
+        private void UpdateChatHistoryThreadSafe(string text)
+        {
+            if (statusAndMsg.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(UpdateChatHistoryThreadSafe);
+                statusAndMsg.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                statusAndMsg.Text += text;
+            }
+        }
+
+        private void UpdateImageThreadSafe(Bitmap bmp)
+        {
+            if (statusAndMsg.InvokeRequired)
+            {
+                var d = new SafeCallDelegateImage(UpdateImageThreadSafe);
+                statusAndMsg.Invoke(d, new object[] { bmp });
+            }
+            else
+            {
+                Clipboard.SetDataObject(bmp);
+                statusAndMsg.Text += "\n";
+                // move cursor to the end of the RTB
+                statusAndMsg.Select(statusAndMsg.Text.Length - 1, 0);
+                // scroll to the end of the RTB
+                statusAndMsg.ScrollToCaret();
+                statusAndMsg.Paste();
+            }
+        }
+
+        #region Responsive
+
+
+
+        #endregion
+    }
+}
